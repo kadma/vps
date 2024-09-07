aa05a8dbfe7e365b5de704f8ac9b9f82b0d87384
kadma <lolocruser@gmail.com>
2024-09-07 18:45:20 -0500

    actualización y eliminación
    se elimina el portainer ya que se puede instalar por medio de easy panel

--- nuevo+docker+easypanel.sh
+++ nuevo+docker+easypanel.sh
@@ -1,12 +1,11 @@
 apt update && apt upgrade -y && apt autoremove -y
+#instala docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -14,6 +13,5 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 apt update && apt upgrade -y && apt autoremove -y
-docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.0
-
+# instala el easy panel
 curl -sSL https://get.easypanel.io | sh
