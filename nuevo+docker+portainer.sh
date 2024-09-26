feb166940c0d0076b397c496fca3ce11278c97af
kadma <lolocruser@gmail.com>
2024-09-26 18:24:53 -0500

    Update portainer
    update portainer to 2.21.2

--- nuevo+docker+portainer.sh
+++ nuevo+docker+portainer.sh
@@ -14,4 +14,4 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 apt update && apt upgrade -y && apt autoremove -y
-docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.2
