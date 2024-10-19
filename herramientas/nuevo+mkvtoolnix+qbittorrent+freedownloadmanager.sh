8808281788623637fa5b461dec6c8c20a44a5af9
kadma <lolocruser@gmail.com>
2024-10-19 12:38:44 -0500

    Update nuevo+mkvtoolnix+qbittorrent+freedownloadmanager.sh

--- herramientas/nuevo+mkvtoolnix+qbittorrent+freedownloadmanager.sh
+++ herramientas/nuevo+mkvtoolnix+qbittorrent+freedownloadmanager.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade -y && apt autoremove -y
 apt install wget -y
 wget -O /usr/share/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 apt update && apt install mkvtoolnix mkvtoolnix-gui qbittorrent -y
 wget -O /root/freedownloadmanager.deb https://files2.freedownloadmanager.org/fdm6_qt5/freedownloadmanager.deb
 apt install xdg-utils ffmpeg -y
